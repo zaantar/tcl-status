735c46f9ae024716f8188f049663624cf9e8d2b8
jan <jan.s@icanlocalize.com>
2016-11-04 20:42:36 +0100

    Add support for the github-updater plugin.

--- tcl-status.php
+++ tcl-status.php
@@ -10,6 +10,7 @@ Text Domain: tcl-status
 Domain Path: /languages
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: zaantar/tcl-status
 */
 
 namespace TclStatus;
